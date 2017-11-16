7b7e6da361936487d78b006af0e519dae67a674e
butterflyace <296572002@qq.com>
2017-11-16 16:23:12 +0800

    数据类型比较

--- src/Demo.java
+++ src/Demo.java
@@ -1,15 +1,19 @@
-import domain.CertType;
-
 public class Demo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-//测试枚举
-        CertType fr = CertType.getByCode("FR");
-        System.out.println(fr);
-        System.out.println(fr.getCode());
-        System.out.println(fr.getMessage());
+//        System.out.println("Hello World!");
+////测试枚举
+//        CertType fr = CertType.getByCode("FR");
+//        System.out.println(fr);
+//        System.out.println(fr.getCode());
+//        System.out.println(fr.getMessage());
 //        System.out.println(fr.getDeclaringClass());
+
+        Integer i = null;//在栈内存创建一个引用i，不指向任何堆地址值
+        Integer j1 = 0;
+        int j2 = 0;//在栈内存创建一个引用j2，指向常量池中的整数0
+        System.out.println(null == j1);//如果用null==0会报错，因为引用数据类型和基本数据类型不能比较，会报错
+
     }
 
 
